146130022edcad2690cd1b955d5048b9daa12dc0
Manull94 <ilya090694@gmail.com>
2021-03-31 18:01:08 +0600

    aga

---
+++ exceptions_notes.py
@@ -0,0 +1,53 @@
+"""
+Заметки по обработчикам ошибок
+"""
+
+# https://docs.python.org/3/library/exceptions.html#exception-hierarchy
+# Иерархия ошибок (неполная)
+# BaseException
+#  +-- KeyboardInterrupt
+#  +-- Exception
+#       +-- StopIteration
+#       +-- StopAsyncIteration
+#       +-- ArithmeticError
+#       |    +-- FloatingPointError
+#       |    +-- OverflowError
+#       |    +-- ZeroDivisionError
+#       +-- AssertionError
+#       +-- AttributeError
+#       +-- EOFError
+#       +-- ImportError
+#       |    +-- ModuleNotFoundError
+#       +-- LookupError
+#       |    +-- IndexError
+#       |    +-- KeyError
+#       +-- MemoryError
+#       +-- NameError
+#       |    +-- UnboundLocalError
+#       +-- OSError
+#       |    +-- BlockingIOError
+#       |    +-- ChildProcessError
+#       |    +-- ConnectionError
+#       |    |    +-- BrokenPipeError
+#       |    |    +-- ConnectionAbortedError
+#       |    |    +-- ConnectionRefusedError
+#       |    |    +-- ConnectionResetError
+#       |    +-- FileExistsError
+#       |    +-- FileNotFoundError
+#       |    +-- InterruptedError
+#       |    +-- IsADirectoryError
+#       |    +-- NotADirectoryError
+#       |    +-- PermissionError
+#       |    +-- ProcessLookupError
+#       |    +-- TimeoutError
+#       +-- ReferenceError
+#       +-- RuntimeError
+#       |    +-- NotImplementedError
+#       |    +-- RecursionError
+#       +-- SystemError
+#       +-- TypeError
+#       +-- ValueError
+#       |    +-- UnicodeError
+#       |         +-- UnicodeDecodeError
+#       |         +-- UnicodeEncodeError
+#       |         +-- UnicodeTranslateError
